9b7b37f9a3f5920b30f70fb4485a99627c779516
Nama Kamu <garuda281209@student.abudzar.sch.id>
2026-09-02 13:58:30 +0700

    Zustand 99% sudah dipelajari

--- src/stores/useProfile.jsx
+++ src/stores/useProfile.jsx
@@ -1,8 +1,10 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-export const useProfile = create ((set) => {
+
+export const useProfile = create (persist((set) => {
     return {
         username: '',
         setUsername: (newUsername) => set({ username: newUsername }),
     }
-})
+}))
