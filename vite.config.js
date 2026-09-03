1bc81ac3cd2d8a7349ed95be1f36e2529314aa94
Nama Kamu <garuda281209@student.abudzar.sch.id>
2026-09-03 13:10:44 +0700

    perbaikan untuk vercel

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import jsconfigPaths from 'vite-jsconfig-paths';
+import path from 'path';
+// import jsconfigPaths from 'vite-jsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), jsconfigPaths()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(import.meta.dirname, './src'),
+    },
+  },
 });
